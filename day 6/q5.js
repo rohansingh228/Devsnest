6d664e73a2cf88a3787f5f541cba26a956b2bd99
rohansingh228 <sanerohansingh@gmail.com>
2021-06-12 18:03:22 +0530

    day 6

--- day 6/q5.js
+++ day 6/q5.js
@@ -21,7 +21,7 @@ function freq(params) {
 const x = freq([3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3]);
 console.log(`${x.maxKey} ( ${x.maxValue} times )`);
 
-
+//Question:
 // Write a JavaScript program to find the most frequent item of an array
 // Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 // Sample Output : a ( 5 times )

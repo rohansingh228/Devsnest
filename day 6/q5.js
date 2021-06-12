87047fea91e768b713d42941444b4fe726515264
rohansingh228 <sanerohansingh@gmail.com>
2021-06-13 00:58:39 +0530

    day 6

--- day 6/q5.js
+++ day 6/q5.js
@@ -1,27 +1,34 @@
-function freq(params) {
-	const newMap = new Map();
-	for (let i = 0; i < params.length; i++) {
-		if (newMap.has(params[i])) {
-			newMap.set(params[i], newMap.get(params[i]) + 1);
-		} else {
-			newMap.set(params[i], 1);
-		}
-	}
-	let maxValue = 0;
-	let maxKey = 0;
-	for (let [key, value] of newMap) {
-		if (value > maxValue) {
-			maxValue = value;
-			maxKey = key;
-		}
-	}
-	return {maxKey, maxValue};
+// var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+function most_freq(arr){
+    
+    let max_count = -1;
+    let max_item;
+    for(let item of arr){
+        let count = 0;
+        for(let innerItem of arr){
+            if(item === innerItem){
+                count +=1;
+            }
+        }
+        if (count > max_count){
+            max_count = count;
+            max_item = item;
+        }
+
+    }
+    // console.log(max_count);
+
+
+    console.log(max_item + " " + '(' + " " + max_count + " " + "times" + " " + ')')
 }
 
-const x = freq([3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3]);
-console.log(`${x.maxKey} ( ${x.maxValue} times )`);
 
-//Question:
+var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+most_freq(arr1);
+
+
+
 // Write a JavaScript program to find the most frequent item of an array
 // Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 // Sample Output : a ( 5 times )

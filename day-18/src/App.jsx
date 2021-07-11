1bd1dbdb6cea2d1a4ff649b47bfedae7a47e8bd4
rohansingh228 <sanerohansingh@gmail.com>
2021-07-11 16:34:25 +0530

    day-18 assignment🚀

---
+++ day-18/src/App.jsx
@@ -0,0 +1,13 @@
+import Chessboard from "./components/ChessBoard";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Chessboard />
+      </div>
+    </div>
+  );
+}
+
+export default App;
